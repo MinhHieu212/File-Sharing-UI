799ec00657d9f417639b4ec400d77c4126dd8c0a
PhucNguyen-1807 <thienphuc532266@gmail.com>
2023-11-06 12:07:26 +0700

    chang to CommonJS

--- p2p/index.js
+++ p2p/index.js
@@ -1,6 +1,7 @@
-import  express from("express")
-const app=express()
+const express = require ("express")
+const cors = require ("cors")
 
+const app=express()
 app.use(cors("*"))
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({extended: false}))
