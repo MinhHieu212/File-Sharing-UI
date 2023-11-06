1059ab7194e0aa178844cfb2c7d8c08061768073
PhucNguyen-1807 <thienphuc532266@gmail.com>
2023-11-06 12:02:00 +0700

    p2p is another server

---
+++ p2p/index.js
@@ -0,0 +1,20 @@
+import  express from("express")
+const app=express()
+
+app.use(cors("*"))
+app.use(express.json({limit: '50mb'}))
+app.use(express.urlencoded({extended: false}))
+const createNode = require("./src/index");
+const node = createNode();
+node.listen(3000,3001,()=>{
+    console.log("node server is running on port 3000 and file server is 3001");
+})
+app.post("/fetch",(req,res)=>{
+    const clientIP=req.body.clientIp
+    const clientPort=req.body.clientPort
+    const fileName=req.body.fileName
+    node.connect(clientIP,Number(clientPort), () => {
+        console.log(`Connection to ${ip} established.`);
+      })
+})
+app.listen("8080",()=>{console.log("im running on 8080");})
