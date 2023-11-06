fc6edc593bf8a4e5b7e939b520032065d5cfd44d
MinhHieu212 <hieu.tranminhk21@hcmut.edu.vn>
2023-11-06 12:47:18 +0700

    save

--- p2p/index.js
+++ p2p/index.js
@@ -1,21 +1,24 @@
-const express = require ("express")
-const cors = require ("cors")
+const express = require("express");
+const cors = require("cors");
 
-const app=express()
-app.use(cors("*"))
-app.use(express.json({limit: '50mb'}))
-app.use(express.urlencoded({extended: false}))
+const app = express();
+app.use(cors("*"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false }));
 const createNode = require("./src/index");
 const node = createNode();
-node.listen(3000,3001,()=>{
-    console.log("node server is running on port 3000 and file server is 3001");
-})
-app.post("/fetch",(req,res)=>{
-    const clientIP=req.body.clientIp
-    const clientPort=req.body.clientPort
-    const fileName=req.body.fileName
-    node.connect(clientIP,Number(clientPort), () => {
-        console.log(`Connection to ${ip} established.`);
-      })
-})
-app.listen("8080",()=>{console.log("im running on 8080");})
+node.listen(3000, 3001, () => {
+  console.log("node server is running on port 3000 and file server is 3001");
+});
+app.post("/fetch", (req, res) => {
+  const clientIP = req.body.clientIp;
+  const clientPort = req.body.clientPort;
+  const fileName = req.body.fileName;
+  console.log(clientIP, "=============================================");
+  node.connect(clientIP, Number(clientPort), () => {
+    console.log(`Connection to ${ip} established.`);
+  });
+});
+app.listen("8080", () => {
+  console.log("im running on 8080");
+});
