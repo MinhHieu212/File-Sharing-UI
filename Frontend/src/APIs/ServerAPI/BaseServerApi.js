d1269d60aeb85228664f0de27a11a1bc0ec55431
PhucNguyen-1807 <thienphuc532266@gmail.com>
2023-11-07 14:14:03 +0700

    get hostDisk

--- Frontend/src/APIs/ServerAPI/BaseServerApi.js
+++ Frontend/src/APIs/ServerAPI/BaseServerApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BaseServerApi = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://172.16.1.156:5000",
   headers: {
     "Content-Type": "application/json",
   },
--- p2p/index.js
+++ p2p/index.js
@@ -28,14 +28,14 @@ app.post("/fetch", (req, res) => {
   setTimeout(() => {
     // chưa giải quyết vấn đề async được nên ta dùng tạm thằng setTimeout
     node.fetchFile({ fileName });
-    res.status(200).send("File Received !!")
+    res.status(200).send("File Received !!");
   }, 1000);
 });
 // sau khi upload thì gọi API lên serverBE để update db
 
 app.post("/uploadRepo", uploadRepo.single("file"), (req, res) => {
   if (!req.file) {
-    return res.status(400).send('No file uploaded.');
+    return res.status(400).send("No file uploaded.");
   }
   return res.status(200).send("Upload to Repo Success fully");
 });
@@ -43,18 +43,20 @@ app.delete("/fileInRepo", (req, res) => {
   const { fileName } = req.body;
   // Check if fileName is provided in the request
   if (!fileName) {
-    return res.status(400).json({ error: 'File name is required in the request body' });
+    return res
+      .status(400)
+      .json({ error: "File name is required in the request body" });
   }
 
   // Construct the path to the file
 
-  const filePath =path.join(__dirname,"./repo",fileName)// Replace this with the actual path to your repository folder
+  const filePath = path.join(__dirname, "./repo", fileName); // Replace this with the actual path to your repository folder
 
   // Use fs.unlink() to delete the file
   fs.unlink(filePath, (err) => {
     if (err) {
-      console.error('Error deleting file:', err);
-      return res.status(500).json({ error: 'Internal server error' });
+      console.error("Error deleting file:", err);
+      return res.status(500).json({ error: "Internal server error" });
     }
     res.status(200).send("File deleted successfully");
   });
@@ -70,21 +72,32 @@ app.get("/hostRepo", (req, res) => {
     return res.status(200).json({ files });
   });
 });
+app.get("/hostDisk", (req, res) => {
+  const folderPath = path.join(__dirname, "../p2p/disk");
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      console.error("Error reading directory:", err);
+      return;
+    }
+    // `files` is an array containing the names of all files in the directory
+    return res.status(200).json({ files });
+  });
+});
 // dùng để publdish lname fanme : quằng file từ disk vào repo
 app.post("/publishDiskToRepo", (req, res) => {
-  const lname=req.body.lname
-  const fname=req.body.fname
+  const lname = req.body.lname;
+  const fname = req.body.fname;
   const sourceFilePath = path.join(__dirname, "disk", lname);
   const destinationFilePath = path.join(__dirname, "repo", fname);
-  fs.rename(sourceFilePath, destinationFilePath, (err) => {
+  fs.copyFile(sourceFilePath, destinationFilePath, (err) => {
     if (err) {
-      console.error('Error moving the file:', err);
-      res.status(400).send({error: err})
+      console.error("Error moving the file:", err);
+      res.status(400).send({ error: err });
     } else {
-      res.status(200).send({message:'File moved and renamed successfully.'});
+      res.status(200).send({ message: "File moved and renamed successfully." });
     }
+  });
 });
-})
 app.listen("8080", () => {
   console.log("P2P server for API is running on 8080");
 });
